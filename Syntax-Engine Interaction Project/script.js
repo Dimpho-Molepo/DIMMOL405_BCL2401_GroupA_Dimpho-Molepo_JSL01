b430d4b2c08be9533d751a9e782998cf9af9476c
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-03-14 18:19:06 +0200

    Added comments on script.js

--- Syntax-Engine Interaction Project/script.js
+++ Syntax-Engine Interaction Project/script.js
@@ -4,12 +4,12 @@ function validateSyntax() {
 
   let result = '';
 
-
+  // Checks if user input matches the correct syntax and displays correct message
   if (input.match(/^pet_[0-9]{4}[a-zA-Z]+$/)) {
       result = "Valid Syntax 🟢";
     } else {
       result = "Invalid Syntax 🔴";
-    }
+  }
 
   document.getElementById('result').innerText = result;
 }
